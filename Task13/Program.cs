e3d4f4750d6e8c5250c53f47fd9d675aa51ced1d
chicheva <chicheva-e@mail.ru>
2022-12-12 21:45:39 +0300

    Задача 13

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает,
+// что третьей цифры нет. Пример: 645-> 5, 78-> нет цифры, 32679-> 6
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+if (num > 100 && num < 1000)
+{
+    num = num % 10;
+    Console.WriteLine($"Трехзначное {num}");
+    return;
+}
+if (num >= 1000)
+{
+    num = num % 100;
+    num = num / 10;
+    Console.WriteLine($"Четырехзначное {num}");
+}
+else {
+    Console.WriteLine($"Нет третьей цифры");
+}
